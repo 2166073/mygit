699e7bf5bfc1b5d2bdef43856e2ffed94855ce4e
2166073 <133201193+2166073@users.noreply.github.com>
2023-05-12 13:42:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Welkom op deze pagina<h1>
+		<?php echo "Hello world"; ?>
+	<?php
+    $nummer=10;
+    $num= 12;
+    echo $nummer+$num;
+	?>
+<?php
+	$nummer= 10;
+	$num= 12;
+	echo  $nummer*$num;
+?>
+<br>
+<?php 
+$naam="Fiza";
+echo "Welkom" .$naam;
+?>
+<br>
+<?php
+
+// deze code bevat veel fouten   haal alle fouten eruit
+//
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam ." zit bij" .$jouwNaam .  "in de klas";
+
+?>
+
+</body>
+</html>
