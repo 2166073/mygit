fbe2fc589f5a43c29105897eb29acabd451e17e1
2166073 <133201193+2166073@users.noreply.github.com>
2023-05-12 14:40:32 +0200

    Add files via upload

---
+++ get.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<form method="POST" action="verwerking.php">
+
+<input type="text" name="naam" placeholder="naam" required> <br>
+
+<input type="text" name="Achternaam" placeholder="Achternaam" required> <br>
+
+<input type="text"  name="leeftijd" placeholder="leeftijd" required> <br>
+
+<input type="email" name="email" placeholder="Email" required> <br>
+
+<input type="email" name="adres" placeholder="adres" required> <br>
+
+<input type="submit" value="Verstuur">
+
+</form>, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
---
+++ post.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<form method="POST" action="verwerking.php">
+
+<input type="text" name="naam" placeholder="naam" required> <br>
+
+<input type="text" name="Achternaam" placeholder="Achternaam" required> <br>
+
+<input type="text"  name="leeftijd" placeholder="leeftijd" required> <br>
+
+<input type="email" name="email" placeholder="Email" required> <br>
+
+<input type="email" name="adres" placeholder="adres" required> <br>
+
+<p> zowel de methode get and post wordt gebruikt voor
+    het verzenden en ontvangen van informatie tussen
+    webclients en servers. Het verschil tussen beide 
+    ligt echter in de zichtbaarheid van hun URL-parameters,
+  die duidelijk voorkomen in de methode get, maar niet in de postmethode.<br>
+  post methode<br>
+  POST requests are never cached<br>
+POST requests do not remain in the browser history<br>
+POST requests cannot be bookmarked<br>
+POST requests have no restrictions on data length.<br>
+get methode<br>
+GET requests can be cached<br>
+GET requests remain in the browser history<br>
+GET requests can be bookmarked<br>
+GET requests should never be used when dealing with sensitive data<br>
+GET requests have length restrictions<br>
+GET requests are only used to request data (not modify)<br>
+<p>
+
+<input type="submit" value="Verstuur">
+
+
+</form>, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
