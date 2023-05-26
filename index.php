fb208f5af5d1537ca0735c9a835e4f4e253de365
2166073 <133201193+2166073@users.noreply.github.com>
2023-05-26 13:40:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-	<h1>Welkom op deze pagina<h1>
-		<?php echo "Hello world"; ?>
-	<?php
-    $nummer=10;
-    $num= 12;
-    echo $nummer+$num;
-	?>
 <?php
-	$nummer= 10;
-	$num= 12;
-	echo  $nummer*$num;
-?>
-<br>
-<?php 
-$naam="Fiza";
-echo "Welkom" .$naam;
-?>
-<br>
-<?php
+  $host = 'localhost:3307';
+ $db   = 'test';
+ $user = 'root';
+ $pass = '';
+ $charset = 'utf8mb4';
+ 
+ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+ $options = [
+	 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	 PDO::ATTR_EMULATE_PREPARES   => false,
+ ];
+ try 
+ {
+	  $pdo = new PDO($dsn, $user, $pass, $options);
+	  echo "Connectie gemaakt! (db_winkel)";
+ } 
+ catch (\PDOException $e) 
+ {
+	  throw new \PDOException($e->getMessage(), (int)$e->getCode());
+ }
 
-// deze code bevat veel fouten   haal alle fouten eruit
-//
-
-$mijnNaam="Carl";
-$jouwNaam="Camel";
-echo $mijnNaam ." zit bij" .$jouwNaam .  "in de klas";
-
-?>
-
+ ?>
 </body>
 </html>
